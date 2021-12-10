f9e7f2b60ff043f221b6e27d59eb913054a42a24
KSV7 <93730979+KSV7@users.noreply.github.com>
2021-12-10 18:08:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,6 @@ import java.util.Optional;
 public class Main {
 	
 	public static void main(String[] args) {
-		// Реализуйте возможность выбора из списка языков программирования (пример в лекции)
-		// язык с заданной сложностью обучения. Верните любой из обладающих нужной
-		// сложностью.
 		ProgrammingLanguage lang1 = new ProgrammingLanguage("Haskell", DifficultyLevel.HARD);
 		ProgrammingLanguage lang2 = new ProgrammingLanguage("Python", DifficultyLevel.EASY);
 		ProgrammingLanguage lang3 = new ProgrammingLanguage("Java", DifficultyLevel.MEDIUM);
